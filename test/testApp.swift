69d83cd1ab7091bfc92685062e092f508c9cad01
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2024-07-22 15:30:18 +0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Hananiah on 2024/7/22.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
